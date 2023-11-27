dd3d6dd133790d1de83266156ff747a52c4efc15
RocioPomaSilvestre <pomarocio05@gmail.com>
2023-11-27 06:34:14 -0400

    27-11-2023

--- connection.js
+++ connection.js
@@ -2,15 +2,15 @@ const mysql = require('mysql');
 
 // Configuración de conexión para el pool
 const pool = mysql.createPool({
-    connectionLimit: 10,
+    connectionLimit: 30,
     //SERVIDOR
-    //host: '181.188.156.195',
+    // host: '181.188.156.195',
     // port: 18006,
 
     host: 'cln5c4yri00q6pmcg18ba0o41',
     user: 'cln5c4yrh0cjzcgpm4icih9ri',
     password: 'c3oLwWprrgZlX1U7nkdv6ZIC',
-    database: 'bdd_proyectos_v03',
+    database: 'bdd_proyectos_v04',
 });
 
 // Obtener una conexión del pool y realizar operaciones
@@ -41,46 +41,3 @@ pool.on('error', (err) => {
 
 // Exportar el pool para su uso en otros módulos
 module.exports = pool;
-
-/*const mysql = require('mysql');
-require('dotenv').config();
-
-try {
-    var connection = mysql.createConnection({
-
-        // port: 18005,
-        // host: '181.188.156.195',
-
-        // SERVIDOR
-        host: '181.188.156.195',
-        port: 18006,
-
-        // host: 'cln5c4yri00q6pmcg18ba0o41',
-        user: 'cln5c4yrh0cjzcgpm4icih9ri',
-        password: 'c3oLwWprrgZlX1U7nkdv6ZIC',
-        database: 'bdd_proyectos_v03',
-
-        // LOCAL
-        // port: 3306, 
-        // host: 'localhost',
-        // user: 'root',
-        // password: '',
-        // database: 'bdd_proyectos_v05'
-
-    });
-
-    connection.connect((err) => {
-        if (!err) {
-            console.log("CONNECTED");
-        }
-        else {
-            console.log('ERROR: ');
-            console.log(err);
-        }
-    })
-
-} catch (error) {
-    console.error('Error de base de datos:', error);
-}
-
-module.exports = connection;*/
